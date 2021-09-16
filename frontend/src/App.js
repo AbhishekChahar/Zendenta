ffcc294da028ea98eabd7e667d3303118bbfafdf
AbhishekChahar <abhishekchaharr@gmail.com>
2021-09-16 14:10:05 +0530

    first commit

---
+++ frontend/src/App.js
@@ -0,0 +1,14 @@
+import Header from "./components/Header/Header";
+import SideBar from "./components/SideBar/SideBar";
+
+function App() {
+  return (
+    <div className="App">
+      <SideBar />
+      <Header />
+
+    </div>
+  );
+}
+
+export default App;
