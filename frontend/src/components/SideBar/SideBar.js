94ed4b067de38b9eb4b15991252fde0a55426009
Naveen-Bhati <bhatin626@gmail.com>
2021-09-19 11:27:45 +0530

    overview layout structured

--- frontend/src/components/SideBar/SideBar.js
+++ frontend/src/components/SideBar/SideBar.js
@@ -1,13 +1,24 @@
 import React from 'react'
+import './_SideBar.scss'
+import Logo from '../../assets/images/logo.png'
+import { Link } from 'react-router-dom'
 
 const SideBar = () => {
+    const style = {
+        textDecoration: "none",
+        color: "inherit"
+    }
     return (
-        <div style={{
-            backgroundColor: 'grey',
-            flex: '0.2',
-            height: "100vh"
-        }}>
-            I am Side Bar
+        <div className='sidebar'>
+            <img src={Logo} alt="" />
+            <ul className='p-0' style={{ paddingLeft: "10px" }}>
+                <li><Link style={style} to='/overview' ><i className="far fa-tachometer-fast"></i> Overview</Link></li>
+                <li><Link style={style} to='/calendar' ><i className="far fa-calendar"></i> Calender</Link></li>
+                <li><Link style={style} to='/patientlist' ><i className="fal fa-user"></i> Patient List</Link></li>
+                <li><Link style={style} to='/messages' ><i className="far fa-sms"></i> Messages</Link></li>
+                <li><Link style={style} to='/paymentinformation' ><i className="far fa-sack-dollar"></i> Payment Info.</Link></li>
+                <li><Link style={style} to='/settings' ><i className="fas fa-sliders-v"></i> Settings</Link></li>
+            </ul>
         </div>
     )
 }
